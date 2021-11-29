4f8b393e4977e4044f8ace31b0cdb17e921e57e2
Rajat Mahajan <mahajanrajat6363@gmail.com>
2021-11-29 16:47:09 +0530

    new commit

--- DemoProject/src/main/java/com/abc/App.java
+++ DemoProject/src/main/java/com/abc/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World from Maven Project by Rajat!" );
-        System.out.println( "Added new line !!!" );
+        System.out.println( "Added new line !!!!!" );
     }
 }
